3c7a4358e0a71c5081ccf00e7acf26b87109f457
valer716 <csikai.valer@gmail.com>
2023-04-16 23:37:11 +0200

    Game osztály dokumentálása

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,29 @@
 import java.util.ArrayList;
 
+/**
+* A Game osztály felelős a játék működéséért. 
+* A játék indítása és befejezése, valamint a körök vezérlése ennek az osztálynak a feladata.
+*/
 public class Game {
+	
+	/**
+	 * A csőrendszert reprezentálja. Tárolja a csöveket, pumpákat, ciszternákat és forrásokat.
+	 */
     private static PipelineSystem pipelineSystem;
+    
+    /**
+     * A játékot játszó szabotőr csapat játékosait tárolja.
+	 */
     private static final ArrayList<Saboteur> saboteurs = new ArrayList<>();
+    
+    /**
+	 * A játékot játszó szerelő csapat játékosait tárolja.
+	 */
     private static final ArrayList<Plumber> plumbers = new ArrayList<>();
 
+    /**
+	 * Elindítja a játékot, betölti a pályát, és elhelyezi a játékosokat véletlenszerűen a csőrendszer komponensein.
+	 */
     public static void StartGame() {
         Skeleton.Call("[Game]", "StartGame()");
         pipelineSystem = new PipelineSystem();
@@ -26,7 +45,19 @@ public class Game {
         }
         Skeleton.Return();
     }
+    
+    /**
+	 * Befejezi a játékot.
+	 */
     public static void EndGame() {}
+    
+    /**
+	 * Új kör kezdődik, meghívja a komponensek Step() metódusát.
+	 */
     public static void NextRound() {}
+    
+    /**
+	 * A körön belül új játékos kerül sorra.
+	 */
     public static void NextPlayer() {}
 }
